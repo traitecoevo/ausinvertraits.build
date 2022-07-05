bf91ea0b647c4a62392cba1e0c58feba90606fdb
payalbal <payalbal@hotmail.com>
2022-07-05 02:16:28 +0000

    adding example studies (as per data extraction sheets)

--- scripts/test.R
+++ scripts/test.R
@@ -1,6 +1,8 @@
 ## Test - upload invert data csv
-rm(list=ls())
+
 .rs.restartR()
+rm(list=ls())
+
 
 ## Setup
 # remotes::install_github("traitecoevo/austraits.build@develop")
@@ -16,8 +18,8 @@ ausinverts <- remake::make("austraits")
 # remake::delete("Cassis_2014") ## delete one study
 # remake::delete("Cassis_2014_config")
 
-## Add studies
-current_study <- "Leijis_2018"
+# Specify an initial study
+current_study <- "Cassis_2014"
 metadata_create_template(current_study)
 metadata_add_traits(current_study)
   ## Add traits > trait_name (as per defintions file)
